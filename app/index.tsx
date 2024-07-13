ce9c36b0d3764a830b3a1c490555ee583b36bb92
Andduril <73158252+Andduril@users.noreply.github.com>
2024-07-13 12:06:32 +0000

    Add styles to app/index.tsx file

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,23 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const Index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Hello world</Text>
+    <View style={styles.container}>
+      <Text style={styles.mainText}>Hello world</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  mainText: {
+    fontSize: 24,
+    color: 'red',
+  },
+});
 
 export default Index;
