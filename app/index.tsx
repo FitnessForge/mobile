6f060456b8f22587300dd5946756523ba461c3eb
Anddy Labrut <73158252+Andduril@users.noreply.github.com>
2024-07-14 22:44:09 +0000

    Add internationalization support

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import i18n from '@/configs/i18n';
+import { I18nextProvider, useTranslation } from 'react-i18next';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 const Index = () => {
+  const { t } = useTranslation();
+
+  const handleLanguageChange = (code: string) => {
+    i18n.changeLanguage(code);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.mainText}>Hello world</Text>
-    </View>
+    <I18nextProvider i18n={i18n}>
+      <View style={styles.container}>
+        <Text style={styles.mainText}>{t('welcome')}</Text>
+        <Button
+          title={t('changeLanguage')}
+          onPress={() =>
+            handleLanguageChange(i18n.language === 'en' ? 'fr' : 'en')
+          }
+        />
+      </View>
+    </I18nextProvider>
   );
 };
 
